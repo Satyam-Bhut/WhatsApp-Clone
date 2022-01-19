ca500053c853cce42d8f14f229508354f5ba820d
Your Name <you@example.com>
2022-01-19 17:15:44 +0530

    Added Delete Functionalities

--- src/Chat.js
+++ src/Chat.js
@@ -1,11 +1,16 @@
 import React,{useState,useEffect} from 'react'
 import './Chat.css'
 import { Avatar,IconButton } from '@material-ui/core' 
-import { AttachFile,MoreVert ,SearchOutlined,Mic,InsertEmoticon } from '@material-ui/icons';
+import {Delete, AttachFile,MoreVert ,Mic,InsertEmoticon } from '@material-ui/icons';
 import { useParams } from 'react-router-dom';
 import db from './firebase'
 import firebase from 'firebase/compat/app'
 import { useStateValue } from './StateProvider';
+
+
+// import { mdiDelete } from '@mdi/js';
+//import RestoreFromTrash from "@bit/mui-org.material-ui-icons.restore-from-trash";
+//import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 
 const Chat = () => {
 
@@ -14,7 +19,7 @@ const Chat = () => {
     const {roomId} = useParams();
     const [roomName,setRoomName] = useState("");
     const [messages,setMessages] = useState([]);
-    // eslint-disable-next-line no-unused-vars
+    
     const [{user},dispatch] = useStateValue();
 
     useEffect(()=>{
@@ -51,6 +56,39 @@ const Chat = () => {
 
         setInput('')
     }
+
+    async function deleteMessage(timestamp){
+        
+        var msgID=2;
+        await db.collection('rooms').doc(roomId).collection('messages').where("timestamp","==",timestamp).get().then((query)=>{
+                query.forEach((data)=>{
+                    msgID=data.id;
+                });
+        });
+        
+        //console.log(msgID);
+        
+        await db.collection('rooms').doc(roomId).collection('messages').doc(msgID.toString()).delete().then(() => {
+            console.log("Document successfully deleted!");
+        }).catch((error) => {
+            console.error("Error removing document: ", error);
+        });
+    }
+
+    async function deleteRoom(){
+    
+        await db.collection('rooms').doc(roomId).delete().then(() => {
+            console.log("Room successfully deleted!");
+        }).catch((error) => {
+            console.error("Error removing Room: ", error);
+        });
+        
+        setRoomName("");
+        setMessages([]);
+
+        window.location.href="/rooms";
+    }
+    
     return (
         <div className='chat'>
                 <div className="chat__header">
@@ -69,27 +107,37 @@ const Chat = () => {
                         <IconButton>
                             <MoreVert />
                         </IconButton>  
-                        <IconButton>
-                            <SearchOutlined />
+                        <IconButton onClick={deleteRoom}>
+                            <Delete />
                        </IconButton>
                     </div>
                     
                 </div>
 
                 <div className="chat__body" id="style-2">
-                      {messages.map(message=>(
-                        <p className={`chat__message ${message.name===user.displayName &&"chat__receiver"}`}>
+                      {
+                         messages.map(message=>{
+                          
+                          //console.log(message.timestamp)
+
+                          return <p key={message.timestamp} className={`chat__message ${message.name===user.displayName &&"chat__receiver"}`}>
                           <span className="chat__name">{message.name}</span>
                           <span className='chat__message__content'>{message.message}</span>
                           <span className="chat__timeStamp">{
                               new Date(message.timestamp?.toDate()).toUTCString()
                           }</span>
+
+                          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+                          <a href='#' onClick={()=>{deleteMessage(message.timestamp)}} className="delete-btn material-icons blue-color"> restore_from_trash</a>
                          </p>
-                      ))}
+                         })
+                      
+                      }
                 </div>
 
                 <div className="chat__footer">
                     <InsertEmoticon />
+                    {/* <Picker onEmojiClick={onEmojiClick} /> */}
                     <form onSubmit={sendMessage}>
                         <input value={input} type="text" onChange={e=>setInput(e.target.value)} placeholder='Type your Message here'/>
                         
